8c72acd5adc18b5c87fa6d73c72e61c9a15ccc37
AfonSir <afonsir17@gmail.com>
2021-11-26 20:38:17 -0300

    Feat: add complement function with nested lambdas

--- closures/functional.rb
+++ closures/functional.rb
@@ -5,6 +5,7 @@ is_even = lambda { |x| x % 2 == 0 }
 is_even.call(4) # true
 is_even.call(5) # false
 
+# version 1
 def complement(predicate)
   lambda do |value|
     !predicate.call(value)
@@ -13,3 +14,13 @@ end
 
 complement(is_even).call(4) # false
 complement(is_even).call(5) # true
+
+# version 2
+complement = lambda do |predicate|
+  lambda do |value|
+    !predicate.call(value)
+  end
+end
+
+complement.call(is_even).call(4) # false
+complement.call(is_even).call(5) # true
