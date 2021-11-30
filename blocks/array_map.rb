7f0cb769ef3bab694fd2f5b52a0068dad794c3c8
AfonSir <afonsir17@gmail.com>
2021-11-30 00:15:22 -0300

    Feat: add array map implementation

---
+++ blocks/array_map.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class Array
+  def each
+    x = 0
+    while x < self.length
+      yield self[x]
+      x += 1
+    end
+  end
+
+  def my_map
+    mapped_array = []
+    each { |item| mapped_array << yield(item) }
+    mapped_array
+  end
+end
+
+new_array = %w(look ma no far loops).my_map do |x|
+  x.upcase
+end
+
+puts new_array.to_s
