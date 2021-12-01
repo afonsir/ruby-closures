18f54966a9c0799e78852ed88949ec3fd9ec9c5b
AfonSir <afonsir17@gmail.com>
2021-12-01 01:37:25 -0300

    Feat: implement to_proc in SpiceGirl class

---
+++ procs/spice_girl.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class SpiceGirl
+  def initialize(name, nick)
+    @name = name
+    @nick = nick
+  end
+
+  def inspect
+    "#{@name} (#{@nick} Spice)"
+  end
+
+  def self.to_proc
+    proc { |obj| new(obj[0], obj[1]).inspect }
+  end
+end
+
+spice_girls = [
+  ['Mel B',  'Scary' ],
+  ['Mel C',  'Sporty'],
+  ['Emma B', 'Baby'  ],
+  ['Geri H', 'Ginger'],
+  ['Vic B',  'Posh'  ]
+]
+
+p spice_girls.map(&SpiceGirl)
